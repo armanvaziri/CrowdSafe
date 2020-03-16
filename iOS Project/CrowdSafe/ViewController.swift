04598d979efe97463a38ea3fb61fb367995c5961
Arman <arman.vaziri1@gmail.com>
2020-03-15 23:18:30 -0700

    csv file reads into array

--- iOS Project/CrowdSafe/ViewController.swift
+++ iOS Project/CrowdSafe/ViewController.swift
@@ -13,12 +13,43 @@ class ViewController: UIViewController {
     @IBOutlet var locationLabel: UILabel!
     @IBOutlet var timeLabel: UILabel!
     @IBOutlet var occupantCountLabel: UILabel!
-    
-    var LiveData: OccupantInfo = OccupantInfo()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        readData()
+    }
+    
+    func readData() -> [[String]] {
+        
+        var result: [[String]] = []
+        
+        let dataFileURL: String? = Bundle.main.path(forResource: "occupancy_data", ofType: "csv")
+        
+        guard (dataFileURL as? String) != nil else { return result}
+            
+        do {
+            
+            let fileUrls = try FileManager.default.contents(atPath: dataFileURL!)
+            
+            let textContent = try String(contentsOfFile: dataFileURL!, encoding: .utf8)
+            
+            let rows = textContent.components(separatedBy: "\n")
+            print("count of rows: ", rows.count)
+            
+            var counter = 1
+            
+            while counter < rows.count - 1 {
+                var row = rows[counter].components(separatedBy: ",")
+                print("row: ", row)
+                result.append(row)
+                counter += 1
+            }
+            return result
+        } catch  {
+            print("error: ", error)
+        }
+        return result
     }
 
 
