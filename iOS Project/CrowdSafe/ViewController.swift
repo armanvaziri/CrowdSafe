065583d319ec8d2362f9c2bf992c4fb686d63c3b
Arman <arman.vaziri1@gmail.com>
2020-03-16 13:02:10 -0700

    uniqueOccupants parsed

--- iOS Project/CrowdSafe/ViewController.swift
+++ iOS Project/CrowdSafe/ViewController.swift
@@ -16,32 +16,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        readData()
+
+//        readData()
+//        print(uniqueOccupantData(file: "occupancy_data"))
+//        print("averge height (mm) : ", occupantAverageHeight(file: "occupancy_data"))
     }
     
-    func readData() -> [[String]] {
+    func readData(file fileName: String) -> [[String]] {
         
         var result: [[String]] = []
-        
-        let dataFileURL: String? = Bundle.main.path(forResource: "occupancy_data", ofType: "csv")
-        
+        let dataFileURL: String? = Bundle.main.path(forResource: fileName, ofType: "csv")
         guard (dataFileURL as? String) != nil else { return result}
             
         do {
-            
-            let fileUrls = try FileManager.default.contents(atPath: dataFileURL!)
-            
             let textContent = try String(contentsOfFile: dataFileURL!, encoding: .utf8)
-            
             let rows = textContent.components(separatedBy: "\n")
-            print("count of rows: ", rows.count)
             
             var counter = 1
-            
             while counter < rows.count - 1 {
                 var row = rows[counter].components(separatedBy: ",")
-                print("row: ", row)
                 result.append(row)
                 counter += 1
             }
@@ -50,6 +43,41 @@ class ViewController: UIViewController {
             print("error: ", error)
         }
         return result
+    }
+    
+    func uniqueOccupantData(file fileName: String) -> [[String]] {
+        let data = readData(file: fileName)
+        
+        var uniqueOccupantID: [String] = []
+        var uniqueOccupantData: [[String]] = []
+        
+        for row in data {
+            if uniqueOccupantID.count == 0 || !(uniqueOccupantID.contains(row[6])) {
+                uniqueOccupantID.append(row[6])
+                uniqueOccupantData.append(row)
+            }
+        }
+        print("number of unique occupants: ", uniqueOccupantData.count)
+        return uniqueOccupantData
+    }
+    
+    func occupantAverageHeight(file fileName: String) -> Int {
+        let uniqueOccupants = uniqueOccupantData(file: fileName)
+    
+        var heightSum = 0
+        
+        for row in uniqueOccupants {
+            heightSum += Int(row[7])!
+        }
+        return heightSum / uniqueOccupants.count
+    }
+    
+    func currentTime() -> String {
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        return "\(hour):\(minutes)"
     }
 
 
